923581bc7ced8c8cdab186c28459526744e684af
Ahmed Toba <ahmdtwbt22@gmail.com>
2025-07-16 20:47:50 +0300

    add day 11 task

---
+++ day11/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import { dbConnection } from "./db/dbconnection.js";
+
+import userRoutes from "./src/modules/user/userRoutes.js";
+import productRoutes from "./src/modules/product/productRoutes.js";
+import cartRoutes from "./src/modules/cart/cartRoutes.js";
+import orderRoutes from "./src/modules/order/orderRoutes.js";
+
+const app = express();
+
+app.use(express.json());
+
+dbConnection;
+
+app.use("/users", userRoutes);
+app.use("/products", productRoutes);
+app.use("/cart", cartRoutes);
+app.use("/orders", orderRoutes);
+
+app.use((req, res) => {
+    res.status(404).json({ message: "Route not found" });
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
---
+++ day12/README.md
@@ -0,0 +1,5 @@
+# Day 12 – Angular Task
+
+This day contains a complete Angular project structured with multiple components and grid layout.
+
+🔗 [Click here to view the project repository](https://github.com/ahmedtoba74/NTI_DAY12_ANGULAR)
